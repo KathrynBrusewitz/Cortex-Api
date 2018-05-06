dc3b4abbb85bf0f907030004b3070b16359d1f68
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-05-05 19:45:57 -0700

    added definition for terms

--- src/models/Term.js
+++ src/models/Term.js
@@ -5,6 +5,7 @@ module.exports = mongoose.model(
   'Term',
   new Schema({
     term: { type: String, required: true },
+    definition: { type: String, default: null },
     description: { type: String, default: null },
   })
 );
